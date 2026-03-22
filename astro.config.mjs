5a0f680037912129a0ec082662a7d5e0f44a0fa3
manuel <xmanuel.mirandax@gmail.com>
2026-03-22 18:48:57 -0400

    actuaizando archivo astro.congi.msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://tu-tienda-laptops.vercel.app',
+  site: 'https://tiendapc.vercel.app/',
   integrations: [sitemap()],
   server: {
     port: 4321,
